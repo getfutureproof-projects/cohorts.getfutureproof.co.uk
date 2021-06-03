edd41c33e9dd8bd43b91c7aa56f8480ca6aab934
Gingertonic <beth@getfutureproof.co.uk>
2021-06-03 22:59:50 +0100

    modal basic content

--- src/layout/Modal/index.js
+++ src/layout/Modal/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useCohort } from '../../contexts/cohort'
 
 export default function Modal() {
-    return (
-        <div id="modal">
+    const cohort = useCohort()
 
+    return (
+        <div id="overlay">
+            <section id="modal">
+                <h1 id="st-name">Name</h1>
+                <div id="icons">
+                    <a id="pop-out" href="https://getfutureproof.co.uk" target="_blank" rel="noreferrer">Open in a new tab</a>
+                </div>
+                <section className="btn-group" id="materials">
+                    <button id="st-video" className="linkout">Video</button>
+                    <button id="st-cv" className="linkout">CV</button>
+                    <button id="st-portfolio" className="linkout">Portfolio</button>
+                </section>
+            </section>
         </div>
     )
 }
