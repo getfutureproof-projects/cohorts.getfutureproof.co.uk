361ad70710788b84507ce4a41c8a6ed214b05176
Gingertonic <beth@getfutureproof.co.uk>
2021-06-03 17:21:04 +0100

    start App comp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css' 
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
-  <h1>Coming Soon!</h1>,
+  <App />,
   document.getElementById('root')
 )
