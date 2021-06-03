6b64950915077a4732aa2231228c5ed607d12f1f
Gingertonic <beth@getfutureproof.co.uk>
2021-06-03 18:24:18 +0100

    Contact styling

--- src/layout/Contact/index.js
+++ src/layout/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './style.css'
 
 export default function Contact({ cohort }) {
     const [ subject, setSubject ] = useState("futureproof Associates")
