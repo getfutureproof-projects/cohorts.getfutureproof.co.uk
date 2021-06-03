4b3ae4770f154a90cdc28ac20226c7dffaddd1a4
Gingertonic <beth@getfutureproof.co.uk>
2021-06-03 23:05:27 +0100

    modal styling

--- src/layout/Modal/index.js
+++ src/layout/Modal/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useCohort } from '../../contexts/cohort'
+import './style.css'
 
 export default function Modal() {
     const cohort = useCohort()
@@ -11,10 +12,15 @@ export default function Modal() {
                 <div id="icons">
                     <a id="pop-out" href="https://getfutureproof.co.uk" target="_blank" rel="noreferrer">Open in a new tab</a>
                 </div>
+
                 <section className="btn-group" id="materials">
                     <button id="st-video" className="linkout">Video</button>
                     <button id="st-cv" className="linkout">CV</button>
                     <button id="st-portfolio" className="linkout">Portfolio</button>
+                </section>
+
+                <section id="content">
+                    <iframe src="" frameborder="0"></iframe>
                 </section>
             </section>
         </div>
