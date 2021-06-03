3e5f855386d51e7f18387006491c79d5e2a82672
Gingertonic <beth@getfutureproof.co.uk>
2021-06-03 18:04:12 +0100

    footer styling

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './style.css'
 
 export default function Footer() {
     return (
-        <>
+        <footer>
             <h3>
                 <a href="http://getfutureproof.co.uk" target="_blank" rel="noopener">getfutureproof.co.uk</a>
             </h3>
-        </>
+        </footer>
     )
 }
