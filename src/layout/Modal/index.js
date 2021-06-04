487ca8a5f6e1af6724c4d5e36031fba0b7bdac28
Gingertonic <beth@getfutureproof.co.uk>
2021-06-04 12:25:16 +0100

    load video and portfolio media from hello_github

--- src/layout/Modal/index.js
+++ src/layout/Modal/index.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useCohort } from '../../contexts/cohort'
 import './style.css'
 
 export default function Modal() {
     const { clearFeatured, featured } = useCohort()
+    const [ media, setMedia ] = useState()
 
-    const renderMaterials = () => featured.materials.map(m => (
-        <button id={`st-${m.type.toLowerCase()}`} className="linkout">
+    useEffect(() => {
+        let video = featured.materials.find(m => m.type === "video")
+        video && setMedia(`https://www.youtube-nocookie.com/embed/${video.url}?start=0&controls=1&autoplay=1`)
+    }, [ featured ])
+
+    const handleSelectMedia = (e, m) => {
+        e.stopPropagation()
+        if ( m.type === 'video'){
+            setMedia(`https://www.youtube-nocookie.com/embed/${m.url}?start=0&controls=1&autoplay=1`)
+        } else {
+            setMedia(m.url)
+        }
+    }
+
+    const renderMaterials = () => featured.materials.map((m, i)=> (
+        <button key={i} id={`st-${m.type.toLowerCase()}`} className="linkout" onClick={(e) => handleSelectMedia(e, m)}>
             {m.type[0].toUpperCase() + m.type.slice(1).toLowerCase()}
         </button>
     ))
@@ -25,7 +40,13 @@ export default function Modal() {
                 </section>
 
                 <section id="content">
-                    <iframe src="" frameBorder="0"></iframe>
+                    <iframe
+                        src={media}
+                        frameBorder="0"
+                        allowFullScreen
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">    
+                    </iframe>
+                    
                 </section>
             </section>
         </div>
