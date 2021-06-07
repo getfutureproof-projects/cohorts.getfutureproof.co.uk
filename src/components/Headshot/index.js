2a9531953f0fedba0f0b48dc3b376e8596a306be
Gingertonic <beth@getfutureproof.co.uk>
2021-06-07 02:14:27 +0100

    dynamic project image url

--- src/components/Headshot/index.js
+++ src/components/Headshot/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useCohort } from '../../contexts/cohort'
 
 export default function Headshot({ person }) {
-    const { feature } = useCohort()
+    const { feature, current } = useCohort()
 
     const handleSelect = (e, toFeature) => {
         e.stopPropagation()
@@ -13,7 +13,7 @@ export default function Headshot({ person }) {
         <div className="img_container" onClick={e => handleSelect(e, person)}>
             { person.project && 
                 <img className="project_logo"
-                    src={`https://futureproof-public-documents.s3.eu-west-2.amazonaws.com/hamilton/projectLogos/${person.project.name.replace(' ', '')}.png`}
+                    src={`https://futureproof-public-documents.s3.eu-west-2.amazonaws.com/${current.name.toLowerCase()}/projectLogos/${person.project.name.replace(' ', '')}.png`}
                     alt={person.project.name}
                     onClick={e => handleSelect(e, person.project)}
                 /> }
