493adb8f6d7fb2eb113754b111367dcd1e3b3732
Gingertonic <beth@getfutureproof.co.uk>
2021-07-02 18:40:42 +0100

    refactor material selection and default to github if no materials

--- src/layout/Modal/index.js
+++ src/layout/Modal/index.js
@@ -8,19 +8,22 @@ export default function Modal() {
 
     
     useEffect(() => {
-        if(featured.materials){
-            let video = featured.materials.find(m => m.type === "video")
-            if(video){
-                setMedia(`https://www.youtube-nocookie.com/embed/${video.url}?start=0&controls=1&autoplay=1`)
-            } else {
-                setMedia(`https://futureproof-public-documents.s3.eu-west-2.amazonaws.com/${current.name.toLowerCase()}/cvs/${normalise(featured.name).replace(/\s/gu, "_")}.pdf`)
+        function selectInitMaterial(){
+            if(featured.materials){
+                let video = featured.materials.find(m => m.type === "video")
+                let cv = featured.materials.find(m => m.type === "cv")
+                if(video) return `https://www.youtube-nocookie.com/embed/${video.url}?start=0&controls=1&autoplay=1`;
+                if(cv) return `https://futureproof-public-documents.s3.eu-west-2.amazonaws.com/${current.name.toLowerCase()}/cvs/${normalise(featured.name).replace(/\s/gu, "_")}.pdf`;
+            } else if (current.projects) {
+                return `https://www.youtube-nocookie.com/embed/${current.projects.videoId}?start=${featured.startPoint}&controls=1&autoplay=1`;
             }
-        } else if (current.projects) {
-            setMedia(`https://www.youtube-nocookie.com/embed/${current.projects.videoId}?start=${featured.startPoint}&controls=1&autoplay=1`)
+            return "github"
         }
+        let media = selectInitMaterial()
+        setMedia(media)
     }, [ featured ])
     
-    const normalise = str => str.normalize("NFD").replace(/\p{Diacritic}/gu, "") 
+    const normalise = str => str.normalize("NFD").replace(/\p{Diacritic}/gu, "")
     
     const handleSelectMedia = (e, m) => {
         e.stopPropagation()
@@ -48,6 +51,25 @@ export default function Modal() {
         </button>
     ))
 
+    const renderContent = () => {
+        if (media ==="github") return renderGitHubStats()
+        return renderIframe()
+        
+    }
+
+    const renderIframe = () => (
+        <iframe
+            src={media}
+            frameBorder="0"
+            allowFullScreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">    
+        </iframe>)
+    
+    const renderGitHubStats = () => (<div id="github-stats">
+        <a href="https://github.com/Gingertonic" target="_blank" rel="noreferrer">Visit GitHub profile <span>➭</span></a>
+        <img src="https://github-readme-stats.vercel.app/api?username=gingertonic&show_icons=true&locale=en" alt="Github stats" />
+    </div>)
+
     const renderStudents = () => featured.students.map((s, i)=> (
         <button key={i} className="linkout" onClick={(e) => handleSelectStudent(e, s)}>
             {s}
@@ -60,7 +82,8 @@ export default function Modal() {
                 <h1 id="st-name">{featured.name}</h1>
                 <div id="icons">
                     <span onClick={clearFeatured} style={{ cursor: "pointer" }}>✖</span>
-                    <a id="pop-out" href={media} target="_blank" rel="noreferrer">Open in a new tab ➭</a>
+                    { media !=="github" && 
+                        <a id="pop-out" href={media} target="_blank" rel="noreferrer">Open in a new tab ➭</a> }
                 </div>
 
                 <section className="btn-group" id="modal-btns">
@@ -69,13 +92,7 @@ export default function Modal() {
                 </section>
 
                 <section id="content">
-                    <iframe
-                        src={media}
-                        frameBorder="0"
-                        allowFullScreen
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">    
-                    </iframe>
-                    
+                    { renderContent() }            
                 </section>
             </section>
         </div>
