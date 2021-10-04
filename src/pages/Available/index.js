02bc95e131ecbc3164425cfc04a1933f0e74ea2a
Gingertonic <beth@getfutureproof.co.uk>
2021-10-05 00:34:08 +0100

    render available with HeadshotsIndex

--- src/pages/Available/index.js
+++ src/pages/Available/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useCohort } from '../../contexts/cohort'
-import { BackBtn, Headshot } from '../../components'
+import { BackBtn, HeadshotsIndex } from '../../components'
 import SEO from '../SEO'
 
 export function Available() {
@@ -10,30 +10,13 @@ export function Available() {
         loadAvailable()
     }, [list])
 
-    const renderHeadshots = () => available.map((s, i) => <Headshot key={i} person={s}/>)
-
     return (
         <>
         <SEO topic="available"/>
         <article id="headshots">
-            { loading && <h2>Finding futureproofers available for interview...</h2> }
+            { loading && <h2>Finding futureproofers available for consultation...</h2> }
             { error && <h2><BackBtn path="/" /> {error}</h2> }
-            { available && (
-                // <section id="container" style={styles}>
-                <section id="container">
-                    <>
-                    <div id="summary_container">
-                        <h2><BackBtn path="/" />
-                                Hello! We are ready to interview now!                
-                        </h2>
-                        <p id="cohort-summary">
-                            Click on our picture to find out more about us.
-                        </p>
-                    </div>
-                    { renderHeadshots() }
-                    </>
-                </section>
-            ) }
+            { available && <HeadshotsIndex showAvailable /> }
         </article>
         </>
     )
