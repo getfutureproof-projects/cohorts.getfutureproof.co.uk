9403ec66753ca69ad543bac52612497a87c75cd7
Gingertonic <beth@getfutureproof.co.uk>
2021-10-06 21:56:35 +0100

    adjust backbtn position

--- src/components/BackBtn/index.js
+++ src/components/BackBtn/index.js
@@ -12,7 +12,7 @@ export default function BackBtn({ path }) {
     }
 
     return (
-        <span id="back-btn" className="btn" onClick={handleClick}>
+        <span id="back-btn" className="btn" onClick={handleClick} style={{position: 'relative', left: '-20px'}}>
             ⇦
         </span>
     )
