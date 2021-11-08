bb27e0b90d0b794459d101d5ba6a6d8e41aa058b
ClaireCullimore <cascullimore@gmail.com>
2021-11-08 15:22:08 +0000

    Updated CV to Profile on Landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -9,7 +9,7 @@ export default function Landing() {
             <SEO topic="index"/>
             <h1 id="greeting">Welcome to futureproof!</h1>
             <div id="summary">
-                <p>Here you can see all our cohorts and find associate CVs, video introductions and portfolio links.</p>
+                <p>Here you can see all our cohorts and find associate profiles, video introductions and portfolio links.</p>
                 <p>For more info on futureproof or any of our associates, contact us below.</p>
             </div>
 
