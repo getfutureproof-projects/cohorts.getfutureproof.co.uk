293bd4b9a38ade0f4b83770523e5104f444a5eef
Beth Michelle <beth@getfutureproof.co.uk>
2022-07-09 18:53:44 +0000

    makes footer responsive

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect }  from 'react'
 import './style.css'
 import { Heading } from '@getfutureproof/fpsb'
 import { ContactBtn } from '../../components'
+import { useWindowSize } from '../../hooks/windowSize';
 
 export default function Footer() {
     const [isVisible, setIsVisible] = useState(false);
+    const screen = useWindowSize()
+    const [ footerStyles, setFooterStyles ] = useState({})
 
     useEffect(() => {   
         window.addEventListener("scroll", listenToScroll);
@@ -25,16 +28,30 @@ export default function Footer() {
         }  
       };
 
+      useEffect(() => {
+        let styles = screen.portrait ? ({
+            outer: { padding: '60px 16px 0', display: 'flex', justifyContent: 'center' },
+            tSpanClass: 'small',
+            bSpanClass: 'tiny'
+        }) : ({
+            outer: { padding: '60px 80px 0', display: 'flex', justifyContent: 'center' },
+            tSpanClass: 'medium',
+            bSpanClass: 'small'
+        }) 
+
+        setFooterStyles(styles)
+    }, [screen])
+
     return (
-        <footer className={isVisible ? 'visible' : 'hidden'} style={{ padding: '60px 80px 0 80px', display: 'flex', justifyContent: 'center' }}>
+        <footer className={isVisible ? 'visible' : 'hidden'} style={footerStyles.outer}>
             {/* <h3>
                 <a href="http://getfutureproof.co.uk" target="_blank" rel="noopener">getfutureproof.co.uk</a>
             </h3> */}
             <div className="footer-container" style={{ width: '100%', maxWidth: '1500px', paddingBottom: '30px', display: 'flex', flexDirection: 'column', gap: '35px' }}>
 
                 <div style={{ textAlign: 'left', display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                    <span className="medium text-body">Still looking for the right candidate?</span>
-                    <span className="small regular">Our expert team can help you build the teams you need to thrive.</span>
+                    <span className={`${footerStyles.tSpanClass} text-body`}>Still looking for the right candidate?</span>
+                    <span className={`${footerStyles.bSpanClass} regular`}>Our expert team can help you build the teams you need to thrive.</span>
                 </div>
 
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
