5a63df41c99028ad185451c60b6efa80a05323d9
Mike Soltysiak <83274523+mtsolt@users.noreply.github.com>
2022-07-12 11:07:54 +0100

    stops the party

--- src/components/Headshot/index.js
+++ src/components/Headshot/index.js
@@ -8,13 +8,36 @@ export default function Headshot({ person, loadStudent, idx, seeMore }) {
     const { feature, current } = useCohort()
     const [cohort, setCohort] = useState("")
     const [showModal, setShowModal] = useState()
+    const [cardShape, setCardShape] = useState('')
+    const [cardColor, setCardColor] = useState('')
     const { pathname } = useLocation()
+
 
     useEffect(() => {
         let cohort = person.cohort || current.name
         let modal = person.cohort || current.showModal
         setCohort(cohort)
         setShowModal(modal)
+    }, [person])
+
+    useEffect(() => {
+        let colors = []
+        for (let i = -1; i < idx; i++) {
+            let opts = ['coral', 'lime', 'lemon', 'violet'];
+            let rand = Math.floor(Math.random() * opts.length);
+            colors.push(opts[rand])
+        }
+        setCardColor(colors)
+    }, [person])
+
+    useEffect(() => {
+        let shapes = []
+        for (let i = -1; i < idx; i++) {
+            let frames = ['angles', 'cog', 'star', 'shield'];
+            let rand = Math.floor(Math.random() * frames.length);
+            shapes.push(frames[rand])
+        }
+        setCardShape(shapes)
     }, [person])
 
     const handleSelect = (e, toFeature) => {
@@ -24,40 +47,37 @@ export default function Headshot({ person, loadStudent, idx, seeMore }) {
 
     const normalise = str => str.normalize("NFD").replace(/\p{Diacritic}/gu, "")
 
-    const setClassNames = () => {
-        let classNames = ["img_container"]
-        showModal && classNames.push("active")
-        return classNames.join(" ")
+    // const setClassNames = () => {
+    //     let classNames = ["img_container"]
+    //     showModal && classNames.push("active")
+    //     return classNames.join(" ")
+    // }
+
+    const chooseColorShape = (arr) => {
+        for (let i = 1; i < arr.length; i++) {
+            return arr[arr.length-1]   
+        }
     }
 
-    const randColor = () => {
-        let opts = ['coral', 'lime', 'lemon', 'violet'];
-        let rand = Math.floor(Math.random() * opts.length);
-        return opts[rand];
-    }
 
-    const randFrame = () => {
-        let frames = ['angles', 'cog', 'star', 'shield'];
-        let rand = Math.floor(Math.random() * frames.length);
-        return frames[rand];
-    }
+
 
     return (
 
         <div
-            className={`bg-${randColor()}`}
+            className={`bg-${chooseColorShape(cardColor)}`}
             style={
                 { display: 'flex', flexWrap: 'wrap', maxWidth: '275px', justifyContent: 'center', height: '325px', padding: '10px 0 20px 0' }
             }
-            color={randColor()}
+            color={chooseColorShape(cardColor)}
         >
             <img
                 width='200px'
                 style={{ objectFit: 'cover' }}
-                className={`framed ${randFrame()}`} src={`${S3_COHORTS}/${cohort.toLowerCase()}/headshots/${normalise(person.name).replace(/\s/gu, '_')}.png`}></img>
+                className={`framed ${chooseColorShape(cardShape)}`} src={`${S3_COHORTS}/${cohort.toLowerCase()}/headshots/${normalise(person.name).replace(/\s/gu, '_')}.png`}></img>
             <span style={{ textAlign: 'center', width: '100%', padding: '5px 0' }}>{person.name}</span>
 
-            { seeMore && <button className="btn bg-purple text-white" onClick={showModal ? (e => handleSelect(e, person)) : undefined}> See more </button> }
+            {seeMore && <button className="btn bg-purple text-white" onClick={showModal ? (e => handleSelect(e, person)) : undefined}> See more </button>}
         </div>
     )
 }
